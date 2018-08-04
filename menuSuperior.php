20723f8e5b0db9f8cc2cef3bbede6369a00d9105
Chayene Goncharenco <37180815+chayeneg@users.noreply.github.com>
2018-08-04 19:02:22 -0300

    menuSuperior

---
+++ menuSuperior.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <title> Menu </title>
+  <link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+</head>
+<body>
+  <nav>
+    <ul class="menu">
+      <li><a href="#"> Home </a></li>
+      <li><a href="#"> Sobre </a></li>
+      <li><a href="#"> O que fazemos? </a></li>
+      <ul>
+        <li><a href="#"> Web Design </a></li>
+        <li><a href="#"> SEO </a></li>
+        <li><a href="#"> Design </a></li>
+      </ul>
+    </li>
+    <li><a href="#"> Links </a></li>
+    <li><a href="#"> Contato </a></li>
+  </ul>
+</nav>
+</body>
+</html>
