238f26b8d26ff8d5ebe02860e8ebe41e650e1636
Rafael Lucas <rafaellucasdacosta42@gmail.com>
2018-08-30 22:12:20 -0300

    Ajustes tela cadastro e conexao com o banco

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = new mysqli("localhost", "root", "", "sistemaWebParaBanco");
+  $conn = new mysqli("localhost", "root", "", "sistemawebparabanco");
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
