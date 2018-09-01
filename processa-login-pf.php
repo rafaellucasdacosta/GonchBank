31b3af80fe65a491a0ff49c3c95cd948176dad73
NatiDomingues <natiellidomingues@gmail.com>
2018-09-01 10:26:01 -0300

    tela de login

--- processa-login-pf.php
+++ processa-login-pf.php
@@ -1,18 +1,19 @@
 <?php
   require_once('conexao.php');
-  $cpf = $_POST['cpf'];
+  $numero_conta = $_POST['numero_conta'];
   $senha = $_POST['senha'];
-  $query = "select cpf, name, id, senha from users where cpf = '$cpf' ";
+  $query = "SELECT numero_conta, titular1, senha, saldo FROM contas WHERE numero_conta = '$numero_conta'";
   if ($result = $conn->query($query)) {
     if ($row = $result->fetch_object()){
       if (strcmp($row->senha, $senha)==0){
         session_start();
-        $_SESSION['cpf'] = $row->cpf;
-        $_SESSION['id'] = $row->id;
-        $_SESSION['name'] = $row->name;
-        header("Location: timeline.php");
+        $_SESSION['numero_conta'] = $row->numero_conta;
+        $_SESSION['titular1'] = $row->titular1;
+        $_SESSION['saldo'] = $row->saldo;
+
+        header('Location: saldo.php');
       } else {
-        echo "<script language'javascript' type='text/javascript'>alert('Login ou senha incorreto(s).');window.location.href='login.php';</script>";
+        echo "<script language'javascript' type='text/javascript'>alert('Login ou senha incorreto(s).');window.location.href='index.php';</script>";
       }
     } else {
       echo "<script language'javascript' type='text/javascript'>alert('Erro desconhecido!');window.location.href='cadastro.php';</script>";
