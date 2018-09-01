0bea9068d236bb823b96aadbe76adf806436f04e
Chayene Goncharenco <cgoncharenco@gmail.com>
2018-09-01 10:01:22 -0300

    Pagina de Cadastro, CSS e Ajustes do Header

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
 <header id="page-header">
   <div id="txt" class="header-login"><strong>Acesse sua conta</strong></div>
   <div id="form-pf" class="header-login">
-    <form id="pessoaf" method="post" action="login.php">
+    <form  action="processa-login-pf.php" id="pessoaf" method="post">
       <label><strong>Pessoa Física</strong></label>
-      <input type="text" name="cpf" placeholder="CPF">
-      <input type="text" name="senha" placeholder="Senha">
+      <input type="text" name="numero_conta" placeholder="Número da conta">
+      <input type="password" name="senha" placeholder="Senha">
       <input type="submit" name="btn_login_pf" value="Acessar">
     </form>
   </div>
   <div id="form-pj" class="header-login">
-    <form id="pessoaj" method="post" action="index.php">
+    <form id="pessoaj" method="post" action="processa-login-pf.php">
       <label><strong>Pessoa Jurídica</strong></label>
-      <input type="text" name="cnpj" placeholder="CNPJ">
+      <input type="text" name="numero_conta" placeholder="Número da conta">
       <input type="text" name="senha" placeholder="Senha">
       <input type="submit" name="btn_login_pf" value="Acessar">
     </form>
