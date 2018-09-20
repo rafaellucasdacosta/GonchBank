1c46e6eac021f6555fc50951da5e488824e6b4f5
Rafael Macedo de Oliveira <rafaelmacedo1995@gmail.com>
2018-09-20 03:54:40 +0000

    Correção de erros da tela de saldo.php

--- saldo.php
+++ saldo.php
@@ -1,72 +1,143 @@
-<?php
-  require_once('verifica-login.php');
-  require_once('conexao.php');
-  $numero_conta = "$auth_numero_conta";
-  $nome = "$auth_nome";
-  $saldo = "$auth_saldo";
-?>
-<html lang="pt">
-<head>
-  <title>Banco</title>
-  <link rel="stylesheet" href="css\style.css" />
-  <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet"/>
-  <script src="js\javascript.js"></script>
-  <?php include("menuSuperior.php"); ?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>GonchBank</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
 
-</head>
-<main>
-  <body>
-    <div class="fancy">
-      <?php
-        echo "<h3>Bem vindo(a)</h3>" . " " . $nome;
-        echo "<h4>Conta</h4>" . " " . $numero_conta;
-        echo "<h4>Saldo atual</h4>" . " " . $saldo;
-      ?>
-      <fieldset>
-        <div class="box-saldo">
-          <h2> Consulta de Saldo e Extrato </h2><BR>
-            <h4>Saldo Atual:</h4><BR>
-              <table  >
-                <tr>
-                  <th> Data </th>
-                  <th> Valor </th>
-                  <th> Autorização </th>
-                </tr>
-                <tr>
-                  <td> 30/08/2018 </td>
-                  <td> R$ 666,00 </td>
-                  <td> 243926 </td>
-                </tr>
-              </table>
-              __________
-              <br><br>
-              <h4>Extrato Últimos 5 dias </h4><BR>
-                <table  >
-                  <tr>
-                    <th> Data </th>
-                    <th> Valor </th>
-                    <th> Descrição </th>
-                    <th> Autorização </th>
-                  </tr>
-                  <tr>
-                    <td> 30/08/2018 </td>
-                    <td> R$ 1.000,00 </td>
-                    <td> Transferencia de Tio Zé </td>
-                    <td> 347382 </td>
-                    <td> + </td>
-                  </tr>
-                  <tr>
-                    <td> 30/08/2018 </td>
-                    <td> R$ 444,00 </td>
-                    <td> Bar do Zé Pretinho </td>
-                    <td> 347386 </td>
-                    <td> - </td>
-                  </tr>
-                </table>
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
 
-              </div>
-            </fieldset>
-          </div>
-        </body>
-        <?php include("footer.php"); ?>
-        </html>
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+	<nav class="colorlib-nav" role="navigation">
+		<div class="top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-2">
+						<div id="colorlib-logo"><a href="index.php">Gonch<span>Bank</span></a></div>
+					</div>
+					<div class="col-md-10 text-right menu-1">
+						<ul>
+							<li class="active"><a href="index.php">Página Inicial</a></li>
+							<li><a href="about.html">Sobre</a></li>
+							<li><a href="contact.html">Contato</a></li>
+							<li class="btn-cta"><a href="login-admin.php"><span>Acesso administrativo</span></a></li>
+								</ul>
+							</li>
+
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</nav>
+
+	<aside id="colorlib-hero" class="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(images/img_bg_1.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner desc">
+		   					<h2 class="heading-section">Sobre Nós</h2>
+		   					<p class="colorlib-lead">Somos um Banco desenvolvido especialmente para você! <a href="#" target="_blank"></a></p>
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		  	</ul>
+	  	</div>
+	</aside>
+		</div>
+	</div>
+
+	<footer id="colorlib-footer" role="contentinfo">
+		<div class="container">
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2018 GonchBank. Todos os direitos reservados. Criado por: <a href="https://colorlib.com/" target="_blank">Colorlib</a></small>
+						<small class="block">Imagens por: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+						</p>
+				</div>
+			</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
